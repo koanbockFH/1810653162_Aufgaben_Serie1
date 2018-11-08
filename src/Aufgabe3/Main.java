532ece9af11c75432fa6ea5f750232079b35799a
Axel Basset <axel23.b@gmail.com>
2018-11-08 13:46:48 +0100

    add comments and finished last point

--- src/Aufgabe3/Main.java
+++ src/Aufgabe3/Main.java
@@ -2,11 +2,16 @@ package Aufgabe3;
 
 public class Main
 {
+    // Ein möglicher Startpunkt der Anwendung
     public static void main(String[] args)
     {
+        //speicher text in variable
         String name = "Axel Basset";
+        //gebe variable aus
         System.out.println(name);
+        //speicher zahl in variable
         int alter = 25;
+        //gebe variable aus
         System.out.println(alter);
     }
 }
--- src/Aufgabe4/Main.java
+++ src/Aufgabe4/Main.java
@@ -2,12 +2,28 @@ package Aufgabe4;
 
 public class Main
 {
-
+    // statische variable der klasse Main, ohne instanz drauf zugreifbar;
     private static String fh_kufstein_tirol;
 
+    // Ein möglicher Startpunkt der Anwendung
     public static void main(String[] args)
     {
         //fh = Fachhochschule
         fh_kufstein_tirol = "Fachhochschule Kufstein Tirol";
+
+        //Gibt Text in die aktuelle Ausgabe(Console) aus --> Escape zeichen \
+        System.out.println( "Java verwendet für Strings\"doppelte Anführungszeichen\"und einzelne Anführungszeichen\n" +
+                "wie 'c' um einzelne Zeichen zu definieren.");
+
+        //Gibt Text in die aktuelle Ausgabe(Console) aus --> Escape zeichen \
+        System.out.println("Durch \\ kann man auch den Backslash selbst beliebig oft ausgeben: \\\\\\\\");
+
+        //Gibt Text in die aktuelle Ausgabe(Console) aus --> Escape zeichen \
+        System.out.println("Ich verwende den\\b Backspace, die Newline\\n und den \\tTabulator");
+
+        //Gibt Text in die aktuelle Ausgabe(Console) aus --> Escape zeichen \
+        System.out.println("Durch den Wagenrücklauf wird die Zeile zurückgesetzt und der Teil nach dem \\r überschreibt\n" +
+                "die bestehende Zeile.\\f ist in der Konsole nicht bemerkbar,da es einen Seitenumbruch\n" +
+                "erzwingt.");
     }
 }
