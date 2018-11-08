6631dab3283047689fc5c392b6f5e450ad955a2c
Axel Basset <axel23.b@gmail.com>
2018-11-08 13:36:23 +0100

    changed delclaration, Aufgabe 5

--- src/Aufgabe4/Main.java
+++ src/Aufgabe4/Main.java
@@ -2,10 +2,12 @@ package Aufgabe4;
 
 public class Main
 {
+
+    private static String fh_kufstein_tirol;
+
     public static void main(String[] args)
     {
         //fh = Fachhochschule
-        final String FH_KUFSTEIN_TIROL = "Fachhochschule Kufstein Tirol";
-
+        fh_kufstein_tirol = "Fachhochschule Kufstein Tirol";
     }
 }
