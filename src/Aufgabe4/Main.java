c5048e347acb0a9d805022e70527b1de5a4319db
Axel Basset <axel23.b@gmail.com>
2018-11-08 13:34:01 +0100

    added new Package, Main Class, and completed Aufgabe 4

---
+++ src/Aufgabe4/Main.java
@@ -0,0 +1,11 @@
+package Aufgabe4;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        //fh = Fachhochschule
+        final String FH_KUFSTEIN_TIROL = "Fachhochschule Kufstein Tirol";
+
+    }
+}
